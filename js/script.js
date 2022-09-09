189999574eb82736be6a5b7bfb402661baf1d4be
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-09-09 10:03:56 -0300

    adicionando tentativas

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 var numeroSecreto = parseInt(Math.random() * 11);
-console.log(numeroSecreto);
+console.log(`O número secreto é ${numeroSecreto}.`);
+
+var tentativas = 1;
+var totalDeTentativas = 3;
 
 function Chutar() {
     var chute = parseInt(document.getElementById("valor").value);
-    console.log(chute);
+    console.log(`Seu chute foi: ${chute}.`);
 
     var elementoResultado = document.getElementById("resultado");
 
     if(chute == numeroSecreto){
-        elementoResultado.innerHTML = "Acertou! O número secreto é " + numeroSecreto + ". Parabéns!!!";
+        elementoResultado.innerHTML = `Parabéns, você acertou! O número secreto é ${numeroSecreto}`;
     } else if (chute > 10 || chute < 0) {
-        elementoResultado.innerHTML = "Por favor, digite um número de 0 a 10.";
-    } else if (chute < numeroSecreto) {
-        elementoResultado.innerHTML = "Errou! O número secreto é maior que " + chute + ". Tente novamente.";
-    } else {
-        elementoResultado.innerHTML = "Errou! O número secreto é menor que " + chute + ". Tente novamente.";
+        elementoResultado.innerHTML = `Por favor, digite um número de 0 a 10.`;
+    } else if (tentativas >= totalDeTentativas) {
+        elementoResultado.innerHTML = `Fim de jogo! Seu número de tentativas acabou. O número secreto era ${numeroSecreto}. Tente novamente.`;
+    }  else {
+        if (chute < numeroSecreto) {
+            elementoResultado.innerHTML = "Errou! O número secreto é maior que " + chute + ". Tente novamente.";
+            tentativas++;
+            console.log(`tentativas: ${tentativas}`);
+        } else {
+            elementoResultado.innerHTML = "Errou! O número secreto é menor que " + chute + ". Tente novamente.";
+            tentativas++;
+        }
     }
 }
 
